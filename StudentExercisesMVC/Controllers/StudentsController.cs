5911729cc96ebdeb630a32d648cf677681c9df73
Panya Farnette <naienko@gmail.com>
2019-05-21 14:57:20 -0500

    in-class group-coding

---
+++ StudentExercisesMVC/Controllers/StudentsController.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using StudentExercisesAPI.Models;
+
+namespace StudentExercisesMVC.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public StudentsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        // GET: Students
+        public ActionResult Index()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+            SELECT s.Id,
+                s.FirstName,
+                s.LastName,
+                s.SlackName,
+                s.CohortId
+            FROM Student s
+        ";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Student> students = new List<Student>();
+                    while (reader.Read())
+                    {
+                        Student student = new Student
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            _firstname = reader.GetString(reader.GetOrdinal("FirstName")),
+                            _lastname = reader.GetString(reader.GetOrdinal("LastName")),
+                            _handle = reader.GetString(reader.GetOrdinal("SlackName")),
+                            _cohortId = reader.GetInt32(reader.GetOrdinal("CohortId"))
+                        };
+
+                        students.Add(student);
+                    }
+
+                    reader.Close();
+
+                    return View(students);
+                }
+            }
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+            SELECT s.Id,
+                s.FirstName,
+                s.LastName,
+                s.SlackName,
+                s.CohortId
+            FROM Student s
+            WHERE s.Id = @StudentId
+        ";
+                    cmd.Parameters.Add(new SqlParameter("@StudentId", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Student student = null;
+                    if (reader.Read())
+                    {
+                        student = new Student
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            _firstname = reader.GetString(reader.GetOrdinal("FirstName")),
+                            _lastname = reader.GetString(reader.GetOrdinal("LastName")),
+                            _handle = reader.GetString(reader.GetOrdinal("SlackName")),
+                            _cohortId = reader.GetInt32(reader.GetOrdinal("CohortId"))
+                        };
+                    }
+
+                    reader.Close();
+
+                    return View(student);
+                }
+            }
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([FromForm] Student student)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Student
+                ( FirstName, LastName, SlackName, CohortId )
+                VALUES
+                ( @firstName, @lastName, @slackName, @cohortId )";
+                    cmd.Parameters.Add(new SqlParameter("@firstName", student._firstname));
+                    cmd.Parameters.Add(new SqlParameter("@lastName", student._lastname));
+                    cmd.Parameters.Add(new SqlParameter("@slackName", student._handle));
+                    cmd.Parameters.Add(new SqlParameter("@cohortId", student._cohortId));
+                    cmd.ExecuteNonQuery();
+
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Students/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- StudentExercisesMVC/Models/Student.cs
+++ StudentExercisesMVC/Models/Student.cs
@@ -7,10 +7,13 @@ namespace StudentExercisesAPI.Models
     {
         public int Id { get; set; }
         [Required]
+        [Display(Name = "First Name")]
         public string _firstname { get; set; }
         [Required]
+        [Display(Name = "Last Name")]
         public string _lastname { get; set; }
         [Required]
+        [Display(Name = "Slack Handle")]
         [StringLength(12, MinimumLength = 3)]
         public string _handle { get; set; }
         public int _cohortId { get; set; }
